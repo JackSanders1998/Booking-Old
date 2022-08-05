40e581c9166d9579b400415c13d5ee28c0641913
Jack Sanders <jacklewissanders@gmail.com>
2022-08-05 17:54:17 -0500

    initialize

--- app/pages/book/[venueId].tsx
+++ app/pages/book/[venueId].tsx
@@ -22,16 +22,8 @@ export const BookVenue = () => {
             </h2>
             <p className="py-2 font-readex font-normal text-base text-slate-11">
               Click on a date in the calendar to see available timeslots. Pick all the timeslots
-              you`&apos;`d be available to play at {venue.name} from the list.
+              you&#39;d be available to play at {venue.name} from the list.
             </p>
-          </div>
-          <div className="w-1/6">
-            <button
-              type="button"
-              className="inline-flex items-center px-1.5 py-1 border border-slate-07 text-base font-normal rounded text-slate-11 bg-slate-03 hover:bg-slate-08 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-08"
-            >
-              Sign in
-            </button>
           </div>
         </div>
         <div className="pt-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-1">
