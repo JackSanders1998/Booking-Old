285d9efaef638cdc9fd80c86b872fef37a8a3fd7
Jack Sanders <jacklewissanders@gmail.com>
2022-08-26 21:27:20 -0500

    link fe to api

--- app/core/components/Modals/AddTimeSlotModal.tsx
+++ app/core/components/Modals/AddTimeSlotModal.tsx
@@ -1,10 +1,146 @@
 import { Fragment, useState } from "react"
 import { Dialog, Transition } from "@headlessui/react"
-import { CheckIcon } from "@heroicons/react/outline"
+import { ClockIcon, TrashIcon } from "@heroicons/react/outline"
+import { ArrowCircleRightIcon } from "@heroicons/react/solid"
+import { add } from "date-fns"
+import { formatInTimeZone, utcToZonedTime } from "date-fns-tz"
+import createTimeSlot from "app/time-slots/mutations/createTimeSlot"
+import { useMutation, useParam, useQuery, useRouter } from "blitz"
+import getVenue from "app/venues/queries/getVenue"
 
-export default function AddTimeSlotModal() {
+const timeZone = "America/Chicago"
+
+interface DatePickerProps {
+  day: Date
+}
+interface TimePickerProps {
+  startOrEnd: "Start" | "End"
+}
+interface TimePicker {
+  hour: number
+  minute: number
+  ampm: string
+}
+
+export default function AddTimeSlotModal(props: DatePickerProps) {
+  console.log(props.day)
+  console.log(typeof props.day)
+  const router = useRouter()
+  const venueId = useParam("venueId", "number")
+  const [venue, { setQueryData, refetch }] = useQuery(
+    getVenue,
+    { id: venueId },
+    {
+      // This ensures the query never refreshes and overwrites the form data while the user is editing.
+      staleTime: Infinity,
+    }
+  )
+  const [createTimeSlotMutation] = useMutation(createTimeSlot)
   const [open, setOpen] = useState(false)
-  console.log
+  const [startTime, setStartTime] = useState<TimePicker>({
+    hour: 9,
+    minute: 30,
+    ampm: "pm",
+  })
+  const [endTime, setEndTime] = useState<TimePicker>({ hour: 11, minute: 45, ampm: "pm" })
+
+  const handleAddGig = async () => {
+    const startDateTime = add(new Date(props.day), {
+      hours: startTime.ampm === "pm" ? startTime.hour + 12 : startTime.hour,
+      minutes: startTime.minute,
+    })
+    const endDateTime = add(new Date(props.day), {
+      hours: endTime.ampm === "pm" ? endTime.hour + 12 : endTime.hour,
+      minutes: endTime.minute,
+    })
+    const values: { start: Date; end: Date; venueId: number } = {
+      start: startDateTime,
+      end: endDateTime,
+      venueId: venue.id,
+    }
+    try {
+      const timeSlot = await createTimeSlotMutation(values)
+      setOpen(false)
+      refetch()
+    } catch (error: any) {
+      console.error(error)
+      return JSON.stringify(error)
+    }
+  }
+  const TimePicker = (props: TimePickerProps): JSX.Element => {
+    const handleChange = (key: any, value: any) => {
+      if (props.startOrEnd === "Start") {
+        startTime[key] = value.target.value
+        setStartTime(startTime)
+      } else if (props.startOrEnd === "End") {
+        endTime[key] = value.target.value
+        setEndTime(endTime)
+      }
+    }
+    return (
+      <div className="flex-auto m-4 p-2 bg-slate-11 rounded-lg text-l">
+        <div className="mx-4 px-2 ">
+          <label
+            htmlFor={`${props.startOrEnd}TimePicker`}
+            className="block text-sm font-medium text-gray-700"
+          >
+            {props.startOrEnd}:
+          </label>
+          <div className="flex">
+            <select
+              id="hour"
+              name="hour"
+              className="flex-1 bg-transparent appearance-none outline-none"
+              defaultValue={props.startOrEnd === "Start" ? startTime.hour : endTime.hour}
+              onChange={(hour: number) => {
+                handleChange("hour", hour)
+              }}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </select>
+            <span className="flex-1">:</span>
+            <select
+              id="minute"
+              name="minute"
+              className="flex-1 bg-transparent appearance-none outline-none"
+              defaultValue={props.startOrEnd === "Start" ? startTime.minute : endTime.minute}
+              onChange={(minute: number) => {
+                handleChange("minute", minute)
+              }}
+            >
+              <option value="00">00</option>
+              <option value="15">15</option>
+              <option value="30">30</option>
+              <option value="45">45</option>
+            </select>
+            <select
+              id="ampm"
+              name="ampm"
+              className="flex-1 bg-transparent appearance-none outline-none"
+              defaultValue={props.startOrEnd === "Start" ? startTime.ampm : endTime.ampm}
+              onChange={(ampm: string) => {
+                handleChange("ampm", ampm)
+              }}
+            >
+              <option value="am">am</option>
+              <option value="pm">pm</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
@@ -40,30 +176,38 @@ export default function AddTimeSlotModal() {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
+                <Dialog.Panel className="relative bg-slate-01 border border-slate-08 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
                   <div>
                     <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-                      <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                      <ClockIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
                     </div>
                     <div className="mt-3 text-center sm:mt-5">
-                      <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                        Payment successful
+                      <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-slate-11">
+                        Add Gigs
                       </Dialog.Title>
                       <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet
-                          labore.
-                        </p>
+                        <p className="text-sm text-gray-500">Select a start and end time.</p>
                       </div>
                     </div>
                   </div>
-                  <div className="mt-5 sm:mt-6">
+                  <div className="flex justify-center mt-2">
+                    <TimePicker startOrEnd="Start" />
+                    <TimePicker startOrEnd="End" />
+                  </div>
+                  <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
                     <button
                       type="button"
-                      className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                      onClick={() => handleAddGig()}
+                    >
+                      Add Gig Time
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
                       onClick={() => setOpen(false)}
                     >
-                      Go back to dashboard
+                      Cancel
                     </button>
                   </div>
                 </Dialog.Panel>
--- app/pages/venues/[venueId]/edit.tsx
+++ app/pages/venues/[venueId]/edit.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from "react"
+import React, { Fragment, Suspense, useState } from "react"
 import { Head, Link, useRouter, useQuery, useMutation, useParam, BlitzPage, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import getVenue from "app/venues/queries/getVenue"
@@ -6,6 +6,82 @@ import updateVenue from "app/venues/mutations/updateVenue"
 import { VenueForm, FORM_ERROR } from "app/venues/components/VenueForm"
 import AddTimeSlotModal from "app/core/components/Modals/AddTimeSlotModal"
 import deleteVenue from "app/venues/mutations/deleteVenue"
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  getDay,
+  isEqual,
+  isSameDay,
+  isSameMonth,
+  isToday,
+  parse,
+  parseISO,
+  startOfToday,
+} from "date-fns"
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DotsVerticalIcon,
+} from "@heroicons/react/solid"
+import { Menu, Transition } from "@headlessui/react"
+import { TimeSlot } from "@prisma/client"
+
+interface CalendarProps {
+  gigs: TimeSlot[]
+}
+
+interface GigProps {
+  gig: TimeSlot
+}
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ")
+}
+
+const CalendarNav = () => {
+  const navBack = () => {
+    console.log("going back!")
+  }
+
+  const navNext = () => {
+    console.log("going next!")
+  }
+
+  return (
+    <div className="flex flex-row-reverse">
+      <button
+        onClick={() => navNext()}
+        type="button"
+        className="text-slate-01 relative rounded-lg bg-violet-11 mx-1 px-3 py-2 shadow-sm flex items-center space-x-3 hover:bg-violet-500 focus-within:ring-2 focus-within:ring-offset-2"
+      >
+        Next
+        <ArrowRightIcon className="h-5 w-5" aria-hidden="true" />
+      </button>
+      <button
+        onClick={() => navBack()}
+        type="button"
+        className="relative rounded-lg border border-slate-06 bg-slate-01 mx-1 px-3 py-2 shadow-sm flex items-center space-x-3 hover:bg-slate-02 focus-within:ring-2 focus-within:ring-offset-2"
+      >
+        <ArrowLeftIcon className="h-5 w-5" aria-hidden="true" />
+        Back
+      </button>
+    </div>
+  )
+}
+
+const Stringify = ({ venue }: any) => {
+  return (
+    <div className="bg-slate-01 border border-slate-06 py-8 px-4 shadow sm:rounded-lg sm:px-10">
+      <div className="px-4 py-5 sm:p-6">
+        return <>{JSON.stringify(venue)}</>
+      </div>
+    </div>
+  )
+}
 
 const DeleteWarning = (venue: any) => {
   const router = useRouter()
@@ -37,6 +113,141 @@ const DeleteWarning = (venue: any) => {
   )
 }
 
+function Gig(props: GigProps) {
+  return (
+    <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
+      <div className="flex-auto">
+        <p className="mt-0.5">
+          <time dateTime={props.gig.start.toDateString()}>{format(props.gig.start, "h:mm a")}</time>{" "}
+          - <time dateTime={props.gig.end.toDateString()}>{format(props.gig.end, "h:mm a")}</time>
+        </p>
+      </div>
+    </li>
+  )
+}
+
+function Calendar(props: CalendarProps) {
+  let today = startOfToday()
+  let [selectedDay, setSelectedDay] = useState(today)
+  let [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"))
+  let firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date())
+
+  let days = eachDayOfInterval({
+    start: firstDayCurrentMonth,
+    end: endOfMonth(firstDayCurrentMonth),
+  })
+
+  function previousMonth() {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 })
+    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"))
+  }
+
+  function nextMonth() {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
+    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"))
+  }
+  let selectedDayGigs = props.gigs.filter((gig) => isSameDay(gig.start, selectedDay))
+  return (
+    <div>
+      <div className="pt-16">
+        <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+          <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
+            <div className="md:pr-14">
+              <div className="flex items-center">
+                <h2 className="flex-auto font-semibold text-slate-11">
+                  {format(firstDayCurrentMonth, "MMMM yyyy")}
+                </h2>
+                <button
+                  type="button"
+                  onClick={previousMonth}
+                  className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Previous month</span>
+                  <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+                <button
+                  onClick={nextMonth}
+                  type="button"
+                  className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Next month</span>
+                  <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
+                <div>S</div>
+                <div>M</div>
+                <div>T</div>
+                <div>W</div>
+                <div>T</div>
+                <div>F</div>
+                <div>S</div>
+              </div>
+              <div className="grid grid-cols-7 mt-2 text-sm">
+                {days.map((day, dayIdx) => (
+                  <div
+                    key={day.toString()}
+                    className={classNames(dayIdx === 0 && colStartClasses[getDay(day)], "py-1.5")}
+                  >
+                    <button
+                      type="button"
+                      onClick={() => setSelectedDay(day)}
+                      className={classNames(
+                        isEqual(day, selectedDay) && "text-white",
+                        !isEqual(day, selectedDay) && isToday(day) && "text-red-500",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-slate-09",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          !isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-gray-400",
+                        isEqual(day, selectedDay) && isToday(day) && "bg-red-500",
+                        isEqual(day, selectedDay) && !isToday(day) && "bg-slate-08",
+                        !isEqual(day, selectedDay) && "hover:bg-gray-200",
+                        (isEqual(day, selectedDay) || isToday(day)) && "font-semibold",
+                        "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
+                      )}
+                    >
+                      <time dateTime={format(day, "yyyy-MM-dd")}>{format(day, "d")}</time>
+                    </button>
+
+                    <div className="w-1 h-1 mx-auto mt-1">
+                      {props.gigs.some((gig) => isSameDay(gig.start, day)) && (
+                        <div className="w-1 h-1 rounded-full bg-sky-500"></div>
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <section className="mt-12 md:mt-0 md:pl-14">
+              <h2 className="font-semibold text-slate-11">
+                Schedule for{" "}
+                <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
+                  {format(selectedDay, "MMM dd, yyy")}
+                </time>
+              </h2>
+              <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+                {selectedDayGigs.length > 0 ? (
+                  selectedDayGigs.map((gig) => <Gig gig={gig} key={gig.id} />)
+                ) : (
+                  <p>No gigs for today.</p>
+                )}
+              </ol>
+            </section>
+          </div>
+        </div>
+      </div>
+      <div>
+        <AddTimeSlotModal day={selectedDay} />
+      </div>
+      <CalendarNav />
+    </div>
+  )
+}
+
 const EditVenue = () => {
   const router = useRouter()
   const venueId = useParam("venueId", "number")
@@ -49,7 +260,6 @@ const EditVenue = () => {
     }
   )
   const [updateVenueMutation] = useMutation(updateVenue)
-  console.log(JSON.stringify(venue, null, 2))
   return (
     <>
       <Head>
@@ -57,6 +267,12 @@ const EditVenue = () => {
       </Head>
       <div className="my-6">
         <form className="space-y-6" action="#" method="POST">
+          <div className="bg-slate-01 border border-slate-06 py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <div className="px-4 py-5 sm:p-6">
+              <Calendar gigs={venue.timeSlots} />
+            </div>
+          </div>
+
           <div className="bg-slate-01 border border-slate-06 py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <div className="md:grid md:grid-cols-3 md:gap-6">
               <div className="md:col-span-1">
@@ -150,6 +366,7 @@ const EditVenue = () => {
           </div>
 
           <DeleteWarning venue={venue} />
+          <Stringify venue={venue} />
 
           <div>
             <VenueForm
@@ -164,17 +381,12 @@ const EditVenue = () => {
                   await setQueryData(updated)
                   router.push(Routes.ShowVenuePage({ venueId: updated.id }))
                 } catch (error: any) {
-                  console.error(error)
                   return {
                     [FORM_ERROR]: error.toString(),
                   }
                 }
               }}
             />
-          </div>
-
-          <div>
-            <AddTimeSlotModal />
           </div>
 
           <div className="flex justify-end">
@@ -197,21 +409,6 @@ const EditVenue = () => {
   )
 }
 
-// export const EditVenue = () => {
-//   const venueId = useParam("venueId", "number")
-//   const [venue, { setQueryData }] = useQuery(
-//     getVenue,
-//     { id: venueId },
-//     {
-//       // This ensures the query never refreshes and overwrites the form data while the user is editing.
-//       staleTime: Infinity,
-//     }
-//   )
-//   const [updateVenueMutation] = useMutation(updateVenue)
-
-//   return <></>
-// }
-
 const EditVenuePage: BlitzPage = () => {
   return (
     <div>
@@ -226,3 +423,13 @@ EditVenuePage.authenticate = true
 EditVenuePage.getLayout = (page) => <Layout>{page}</Layout>
 
 export default EditVenuePage
+
+let colStartClasses = [
+  "",
+  "col-start-2",
+  "col-start-3",
+  "col-start-4",
+  "col-start-5",
+  "col-start-6",
+  "col-start-7",
+]
